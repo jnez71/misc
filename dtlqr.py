98736fa23257aa9453231ab6e5a9154dd71fd5f6
jnez71 <nez.jason7@gmail.com>
2017-10-29 00:07:50 -0400

    DTLQR: more interesting trajectory

--- dtlqr.py
+++ dtlqr.py
@@ -17,13 +17,13 @@ n_t = len(T)
 
 # A = df/dx, x_next = f(x, u) ~= A*x + B*u
 n_x = 2
-A = np.array([[  1,   dt],
-              [-dt, 1-dt]])
+A = np.eye(n_x) + np.array([[ 0,  1],
+                            [-1, -1]])*dt
 
 # B = df/du
 n_u = 1
-B = np.array([[ 0],
-              [dt]])
+B = np.array([[0],
+              [1]])*dt
 
 # Control limits
 u_lims = (-10, 10)
@@ -42,9 +42,10 @@ for i in xrange(n_t-1):
 L = np.vstack(map(lambda L: np.hstack(L[::-1]), L[::-1]))
 Apows = np.vstack(Apows)
 
-# Initial condition and desired state trajectory
-x0 = [1, 0]
-Xr = np.array([[3, 0]] * n_t)
+# Desired state trajectory and initial condition
+per = 3; amp = 3
+Xr = np.vstack((amp*np.cos(2*np.pi/per * T), -amp*2*np.pi/per*np.sin(2*np.pi/per * T))).T
+x0 = Xr[0]
 
 # Optimal control
 effort_weight = 0.001
@@ -55,33 +56,33 @@ Ly = L.T.dot(y)
 cost = lambda U: np.sum((L.dot(U) - y)**2) + effort_weight*np.sum(U**2)
 cost_jac = lambda U: 2*(LL.dot(U) - Ly + effort_weight*U.T)
 cost_hess = lambda U: 2*(LL + effort_weight*np.eye(n_tu))
-opt = minimize(fun=cost, x0=np.zeros((n_tu, 1)), method="SLSQP", jac=cost_jac, hess=cost_hess, bounds=[u_lims]*n_tu)  # use Newton-CG if hess
+opt = minimize(fun=cost, x0=np.zeros((n_tu, 1)), method="SLSQP", jac=cost_jac, hess=cost_hess, bounds=[u_lims]*n_tu)  # use Newton-CG if no bounds
 print "Optimization Success: {}".format(opt.success)
 U = opt.x.reshape((n_t, n_u))
-# U = npl.pinv(L).dot(y).reshape((n_t, n_u))  # quadratic-cost, unconstrained simple analytical solution
 
 # Simulation (iterative, as verification of L)
-# K = np.array([[20, 10]]); U = []  # uncomment for naive hand-tuned controller
-X = np.zeros((n_t, n_x))
+X = np.zeros((n_t, n_x)); X[0] = x0
+U_act = [np.clip(U[0], u_lims[0], u_lims[1])]
 for i, t in enumerate(T[1:]):
-    # U.append(np.clip(K.dot(Xr[i] - X[i]), u_lims[0], u_lims[1]))  # uncomment for naive hand-tuned controller
-    X[i+1] = A.dot(X[i]) + B.dot(np.clip(U[i], u_lims[0], u_lims[1]))
-# U.append([0])  # uncomment for naive hand-tuned controller
+    U_act.append(np.clip(U[i+1], u_lims[0], u_lims[1]))
+    X[i+1] = A.dot(X[i]) + B.dot(U_act[i])
 
 # Visualization
 fig, (ax0, ax1) = pyplot.subplots(2, 1)
 fig.suptitle("Result", fontsize=16)
 ax0.plot(T, X[:, 0], 'k', label="Position")
 ax0.plot(T, X[:, 1], 'b', label="Velocity")
-ax0.plot(T, Xr[:, 0], 'k--', label="Desired Position")
-ax0.plot(T, Xr[:, 1], 'b--', label="Desired Velocity")
+ax0.plot(T, Xr[:, 0], 'k--', label="Position Desired")
+ax0.plot(T, Xr[:, 1], 'b--', label="Velocity Desired")
 ax0.set_xlim(T[0], T[-1])
 ax0.set_ylabel("State", fontsize=16)
 ax0.grid(True)
-ax0.legend(fontsize=16)
-ax1.plot(T, U, 'r')
+ax0.legend(fontsize=10)
+ax1.plot(T, U_act, 'r', label="Effort Achieved")
+ax1.plot(T, U, 'r--', label="Effort Requested")
 ax1.set_xlim(T[0], T[-1])
 ax1.set_ylabel("Effort", fontsize=16)
 ax1.set_xlabel("Time", fontsize=16)
 ax1.grid(True)
+ax1.legend(fontsize=10)
 pyplot.show()
